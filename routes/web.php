47421d7b879a4344dd94222d454ea12d08e5799f
moises-devfametro <moises.rodrigues@unifametro.edu.br>
2020-01-12 20:42:08 -0300

    autenticacao com JTW concluido

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,18 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['prefix' => 'api/v1/'], function () use ($router) {
+$router->group(['prefix' => 'api/v1/', 'middleware' => 'auth'], function () use ($router) {
     $router->get('chamados/aberto', 'ChamadoController@getAllAberto');
+
     $router->get('chamados/aberto/hoje', 'ChamadoController@getAllAbertoHoje');
+
     $router->get('chamados/pendente', 'ChamadoController@getAllPendente');
+
     $router->get('chamados/concluido/hoje', 'ChamadoController@getAllConcluidoHoje');
 
     $router->get('chamado/{id}', 'ChamadoController@get');
+
     $router->post('chamado/criar', 'ChamadoController@store');
+
     $router->put('chamado/atualizar/{id}', 'ChamadoController@update');
 });
