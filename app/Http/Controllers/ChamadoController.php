060db1c8cc3dae88ef63c74a62be22456fa6834e
moises-devfametro <moises.rodrigues@unifametro.edu.br>
2020-01-12 18:27:31 -0300

    implementado metodos de recuperacao dos dados e joins

--- app/Http/Controllers/ChamadoController.php
+++ app/Http/Controllers/ChamadoController.php
@@ -15,36 +15,49 @@ class ChamadoController extends Controller
         //
     }
     public function getAllAberto(){
-        $chamado = DB::table('solicitacao')
-            ->join('aluno', 'solicitacao.aluno_id', '=', 'aluno.id')
-            ->join('status', 'solicitacao.status_id', '=', 'status.id')
-            ->join('curso', 'aluno.curso_id', '=', 'curso.id')
-            ->join('tipocurso', 'curso.tipocurso_id', '=', 'tipocurso.id')
-            ->select('solicitacao.id', 'solicitacao.data', 'aluno.nome', 'aluno.cpf', 'status.descricao')
+        $chamado = DB::table('solicitacao AS SOLIC')
+            ->join('status', 'SOLIC.status_id', '=', 'status.id')
+            ->join('aluno AS ALU', 'SOLIC.aluno_id', '=', 'ALU.id')
+            ->join('curso', 'curso.id', '=', 'ALU.curso_id')
+            ->where('status.descricao', '=', 'Aberto')
+            ->select('SOLIC.id', 'ALU.nome', 'ALU.cpf', 'curso.nome AS curso', 'SOLIC.contato', 'SOLIC.wifi_id', 'SOLIC.portal_id', 'SOLIC.email_id', 'SOLIC.observacao', 'status.descricao AS status')
             ->get();
-        // $chamado = Chamado::where('status_id', 1)
-        //        ->orderBy('data', 'desc')
-        //        ->get();
         return response()->json($chamado);
     }
     public function getAllAbertoHoje(){
-        $users = Chamado::whereDate('data', date('Y-m-d'))
-                ->get();
-        return response()->json($users);
+        // $chamado = Chamado::whereDate('data', date('Y-m-d'))
+        //         ->get();
+        // return response()->json($chamado);
     }
     public function getAllPendente(){
-        $chamado = Chamado::where('status_id', 2)
-                ->orderBy('data', 'desc')
-                ->get();
+        $chamado = DB::table('solicitacao AS SOLIC')
+            ->join('status', 'SOLIC.status_id', '=', 'status.id')
+            ->join('aluno AS ALU', 'SOLIC.aluno_id', '=', 'ALU.id')
+            ->join('curso', 'curso.id', '=', 'ALU.curso_id')
+            ->where('status.descricao', '=', 'Pendente')
+            ->select('SOLIC.id', 'ALU.nome', 'ALU.cpf', 'curso.nome AS curso', 'SOLIC.contato', 'SOLIC.wifi_id', 'SOLIC.portal_id', 'SOLIC.email_id', 'SOLIC.observacao', 'status.descricao AS status', 'SOLIC.data AS created_at')
+            ->get();
         return response()->json($chamado);
     }
     public function getAllConcluidoHoje(){
-        $users = Chamado::whereDate('encerramentochamado', date('Y-m-d'))
-                ->get();
-    return response()->json($users);
+        $chamado = DB::table('solicitacao AS SOLIC')
+            ->join('status', 'SOLIC.status_id', '=', 'status.id')
+            ->join('aluno AS ALU', 'SOLIC.aluno_id', '=', 'ALU.id')
+            ->join('curso', 'curso.id', '=', 'ALU.curso_id')
+            ->whereDate('encerramentochamado', date('Y-m-d'))
+            ->select('SOLIC.id', 'ALU.nome', 'ALU.cpf', 'curso.nome AS curso', 'SOLIC.contato', 'SOLIC.wifi_id', 'SOLIC.portal_id', 'SOLIC.email_id', 'SOLIC.observacao', 'status.descricao AS status')
+            ->get();
+        return response()->json($chamado);
     }
     public function get($id){
-        return response()->json(Chamado::find($id));
+        $chamado = DB::table('solicitacao AS SOLIC')
+            ->join('status', 'SOLIC.status_id', '=', 'status.id')
+            ->join('aluno AS ALU', 'SOLIC.aluno_id', '=', 'ALU.id')
+            ->join('curso', 'curso.id', '=', 'ALU.curso_id')
+            ->where('SOLIC.ID', '=', $id)
+            ->select('SOLIC.id', 'ALU.nome', 'ALU.cpf', 'curso.nome AS curso', 'SOLIC.contato', 'SOLIC.wifi_id', 'SOLIC.portal_id', 'SOLIC.email_id', 'SOLIC.observacao', 'status.descricao AS status', 'SOLIC.data AS created_at')
+            ->get();
+            return response()->json($chamado);
     }
     public function store(Request $request){
         $chamado = Chamado::create($request->all()); 
